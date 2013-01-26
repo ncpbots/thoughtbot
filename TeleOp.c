6cb3ba79bd821a4064561797b79df02ef07ad62e
Eric Tendian <erictendian@gmail.com>
2013-01-25 23:33:44 -0600

    completely rebuilt robot, with new wiring

--- TeleOp.c
+++ TeleOp.c
@@ -1,11 +1,11 @@
 #pragma config(Hubs,  S1, HTMotor,  HTMotor,  HTMotor,  HTServo)
 #pragma config(Sensor, S1,     ,               sensorI2CMuxController)
-#pragma config(Motor,  mtr_S1_C1_1,     frontLeft,     tmotorTetrix, openLoop, reversed)
-#pragma config(Motor,  mtr_S1_C1_2,     rearLeft,      tmotorTetrix, openLoop, reversed)
-#pragma config(Motor,  mtr_S1_C2_1,     frontRight,    tmotorTetrix, openLoop)
-#pragma config(Motor,  mtr_S1_C2_2,     rearRight,     tmotorTetrix, openLoop)
-#pragma config(Motor,  mtr_S1_C3_1,     light,         tmotorTetrix, openLoop)
-#pragma config(Motor,  mtr_S1_C3_2,     lift,          tmotorTetrix, openLoop)
+#pragma config(Motor,  mtr_S1_C1_1,     rearRight,     tmotorTetrix, openLoop)
+#pragma config(Motor,  mtr_S1_C1_2,     frontRight,    tmotorTetrix, openLoop)
+#pragma config(Motor,  mtr_S1_C2_1,     light,         tmotorTetrix, openLoop)
+#pragma config(Motor,  mtr_S1_C2_2,     lift,          tmotorTetrix, openLoop)
+#pragma config(Motor,  mtr_S1_C3_1,     frontLeft,     tmotorTetrix, openLoop)
+#pragma config(Motor,  mtr_S1_C3_2,     rearLeft,      tmotorTetrix, openLoop)
 #pragma config(Servo,  srvo_S1_C4_1,    claw,                 tServoStandard)
 #pragma config(Servo,  srvo_S1_C4_2,    servo2,               tServoNone)
 #pragma config(Servo,  srvo_S1_C4_3,    servo3,               tServoNone)
@@ -19,16 +19,23 @@
 /*
 -------------------TEAM 5359 TELEOP PROGRAM-------------------
 main robot movement:
-left stick controls left motors
-right stick controls right motors
+	left stick:
+		- up -> forward
+		- down -> backward
+		- left -> strafe left
+		- right -> strafe right
+	right stick:
+		- left -> turn left
+		- right -> turn right
 
 lift movement:
-D-pad up moves lift up
-D-pad down moves lit down
+	D-pad up moves lift up
+	D-pad down moves lit down
 
 claw open/close: button 5 opens, button 6 closes
 
-lights: hold 2 for light
+lights: 2 to turn on, 4 to turn off
+
 */
 
 task main()
@@ -40,25 +47,79 @@ task main()
   {
 	  getJoystickSettings(joystick);
 
-	  //MAIN ROBOT CONTROL
-	  if (abs(joystick.joy1_y1)>10) {
-		  motor[frontLeft] = joystick.joy1_y1;
-		  motor[rearLeft] = joystick.joy1_y1;
-		}
-		if (abs(joystick.joy1_y2)>10) {
-		  motor[frontRight] = joystick.joy1_y2;
-		  motor[rearRight] = joystick.joy1_y2;
+	  //------------MAIN ROBOT CONTROL------------//
+
+	  //====LEFT JOYSTICK====//
+
+	  //***FORWARD***//
+	  if (joystick.joy1_y1>10 && abs(joystick.joy1_x1)<10 && abs(joystick.joy1_x2)<10)
+		{
+		  motor[frontLeft] = -abs(joystick.joy1_y1); //counterclockwise
+		  motor[frontRight] = abs(joystick.joy1_y1); //clockwise
+		  motor[rearLeft]  = -abs(joystick.joy1_y1); //counterclockwise
+		  motor[rearRight]	= abs(joystick.joy1_y1); //clockwise
 		}
 
-	  //LIFT CONTROL
+	  //***REVERSE***//
+		if (joystick.joy1_y1<-10 && abs(joystick.joy1_x1)<10 && abs(joystick.joy1_x2)<10)
+		{
+		  motor[frontLeft] =	 abs(joystick.joy1_y1); //counterclockwise
+		  motor[frontRight] = -abs(joystick.joy1_y1); //clockwise
+		  motor[rearLeft] = 	 abs(joystick.joy1_y1); //counterclockwise
+		  motor[rearRight] =  -abs(joystick.joy1_y1); //clockwise
+		}
+
+	  //***STRAFE LEFT***//
+		if (joystick.joy1_x1>10 && abs(joystick.joy1_x1)<10 && abs(joystick.joy1_x2)<10)
+		{
+		  motor[frontLeft] =  abs(joystick.joy1_x1); //counterclockwise
+		  motor[frontRight] = abs(joystick.joy1_x1); //clockwise
+		  motor[rearLeft]  = -abs(joystick.joy1_x1); //counterclockwise
+		  motor[rearRight] = -abs(joystick.joy1_x1); //clockwise
+		}
+
+	  //***STRAFE RIGHT***//
+	  if (joystick.joy1_x1<-10 && abs(joystick.joy1_x1)<10 && abs(joystick.joy1_x2)<10)
+		{
+		  motor[frontLeft] = 	-abs(joystick.joy1_x1); //counterclockwise
+		  motor[frontRight] = -abs(joystick.joy1_x1); //clockwise
+		  motor[rearLeft] 	=  abs(joystick.joy1_x1); //counterclockwise
+		  motor[rearRight] = 	 abs(joystick.joy1_x1); //clockwise
+		}
+
+		//====LEFT JOYSTICK====//
+
+		//***TURN LEFT***//
+		if (joystick.joy1_x2>10 && abs(joystick.joy1_x1)<10 && abs(joystick.joy1_y1)<10)
+		{
+		  motor[frontLeft] =  abs(joystick.joy1_x2); //clockwise
+		  motor[frontRight] = abs(joystick.joy1_x2); //clockwise
+		  motor[rearLeft]  =  abs(joystick.joy1_x2); //clockwise
+		  motor[rearRight] =  abs(joystick.joy1_x2); //clockwise
+		}
+
+	  //***TURN RIGHT***//
+	  if (joystick.joy1_x2<-10 && abs(joystick.joy1_x1)<10 && abs(joystick.joy1_y1)<10)
+		{
+		  motor[frontLeft] = 	-abs(joystick.joy1_x2); //counterclockwise
+		  motor[frontRight] = -abs(joystick.joy1_x2); //counterclockwise
+		  motor[rearLeft]  =  -abs(joystick.joy1_x2); //counterclockwise
+		  motor[rearRight] = 	-abs(joystick.joy1_x2); //counterclockwise
+		}
+
+
+
+	  //--------------LIFT CONTROL----------------//
 	  if (joystick.joy1_TopHat == 0) motor[lift] = 40; //move up with D-pad up
 	  else if (joystick.joy1_TopHat == 4) motor[lift] = -40; //move down with D-pad down
 
-	  //CLAW CONTROL
-	  if (joy1Btn(5)==1) servo[claw] = 205; //open on pressing 5
+	  //--------------CLAW CONTROL----------------//
+	  if (joy1Btn(5)==1) servo[claw] = 140; //open on pressing 5
 	  else if (joy1Btn(6)==1) servo[claw] = 220; //close on pressing 6
 
-		//LIGHTS ON/OFF
-	  if (joy1Btn(2)==1) motor[light] = 50; //hold down 2 for lights
+		//--------------LIGHTS ON/OFF---------------//
+	  if (joy1Btn(2)==1) motor[light] = 50; //press 2 to turn on lights
+	 	if (joy1Btn(4)==1) motor[light] = 0; //press 4 to turn off lights
+
   }
 }
