d675525cd4c701315b1f0ef29904fad1b14f042d
Alvin <ling94alvin@gmail.com>
2013-01-27 00:24:22 -0600

    Changed Diagonal Strafe
    Figured I should just take the average, otherwise power input for the
    motors would exceed 100.

--- TeleOp.c
+++ TeleOp.c
@@ -16,187 +16,175 @@
 
 #include "JoystickDriver.c"  //Include file to "handle" the Bluetooth messages.
 
-/*
--------------------TEAM 5359 TELEOP PROGRAM-------------------
-main robot movement:
-	left stick:
-		- up -> forward
-		- down -> backward
-		- left -> strafe left
-		- right -> strafe right
-		- left-up -> strafe northwest
-		- right-up -> strafe northeast
-		- left-down -> strafe southwest
-		- right-down -> strafe southeast
-	right stick:
-		- left -> turn left
-		- right -> turn right
+/*--------------------------------------------------------------------------------------------------------*\
+|*                                                                                                        *|
+|*                                    - FTC Team 5359 [SolinNoids] -                                      *|
+|*                                           Tele-Op Program                                              *|
+|*                                                                                                        *|
+|*  This program is for the holonomic drive setup for the Team 5359's 2012-13 FTC Robot.                  *|
+|*  This setup uses four omniwheels positions at 45 degree angles at the corners of the robot.            *|
+|*  ____________________________________________________________________________________________________  *|
+|*                                                                                                        *|
+|*                                              CONTROLS                                                  *|
+|*     Main Robot Movement:                                                                               *|
+|*         Left Joystick:                                                                                 *|
+|*            - Up -------------> Foward                                                                  *|
+|*            - Down -----------> Backward                                                                *|
+|*            - Left -----------> Strafe Left                                                             *|
+|*            - Right ----------> Strafe Right                                                            *|
+|*            - Left+Up --------> Strafe Forward-Left                                                     *|
+|*            - Right+Up -------> Strafe Forward-Right                                                    *|
+|*            - Left+Down ------> Strafe Back-Left                                                        *|
+|*            - Right+Down -----> Strafe Back-Right                                                       *|
+|*         Right Stick:                                                                                   *|
+|*            - Left -----------> Rotate Left/Anti-Clockwise                                              *|
+|*            - Right ----------> Rotate Right/Clockwise                                                  *|
+|*                                                                                                        *|
+|*     Lift Movement:                                                                                     *|
+|*            - D-Pad Up -------> Move lift up                                                            *|
+|*            - D-Pad Down -----> Move lift down                                                          *|
+|*                                                                                                        *|
+|*     Claw Controls                                                                                      *|
+|*            - Num-Button 5 ---> Claw open                                                               *|
+|*            - Num-Button 6 ---> Claw close                                                              *|
+|*            - Num-Button 2 ---> Lights on                                                               *|
+|*            - Num-Button 4 ---> Lights off                                                              *|
+|*                                                                                                        *|
+\*---------------------------------------------------------------------------------------------------5359-*/
 
-lift movement:
-	D-pad up moves lift up
-	D-pad down moves lit down
+//------------------------------------------ ULTILITY METHODS --------------------------------------------//
 
-claw open/close: button 5 opens, button 6 closes
+int xClock()  {return abs(joystick.joy1_x1);}		//Left joystick x-axis value; clockwise
+int xAClock() {return -abs(joystick.joy1_x1);}	//Left joystick x-axis value; anti-clockwise
+int yClock()  {return abs(joystick.joy1_y1);}		//Left joystick y-axis value; clockwise
+int yAClock() {return -abs(joystick.joy1_y1);}	//Left joystick y-axis value; anti-clockwise
 
-lights: 2 to turn on, 4 to turn off
+/* Averages the left joystick x and y axis values. */
+int joyAvg()	{return (joystick.joy1_x1 + joystick.joy1_y1) / 2;}
 
-*/
-
-void stopMotors() {
-	motor[frontLeft] = 0; //off
-	motor[frontRight]= 0; //off
-	motor[rearLeft]  = 0; //off
-	motor[rearRight] = 0; //off
+/* Moves wheel motors.
+ * @param:
+ * 	- fl: input for frontLeft motor
+ * 	- fr: input for frontRight motor
+ * 	- rl: input for rearLeft motor
+ * 	- rr: input for rearRight motor
+ */
+void moveWheels(int fl, int fr, int rl, int rr)
+{
+	motor[frontLeft]  = fl;
+	motor[frontRight] = fr;
+	motor[rearLeft]   = rl;
+	motor[rearRight]  = rr;
 }
 
-int joyDistance() {
-	return sqrt(joystick.joy1_x1^2+joystick.joy1_y1^2);
+/* Moves all wheel motors.
+ * @param:
+ * 	- joyValue: input for frontLeft motor
+ */
+void moveWheels(int joyValue)
+{
+   motor[frontLeft]  = joyValue;
+	motor[frontRight] = joyValue;
+	motor[rearLeft]   = joyValue;
+	motor[rearRight]  = joyValue;
 }
+
+/* Moves lift motors.
+ * @param:
+ * 	- power: power allocated to lift motors
+ */
+void moveLift(int power)
+{
+   motor[lift1] = power;
+   motor[lift2] = power;
+}
+//------------------------------------------ END ULTILITY METHODS ----------------------------------------//
+
+
+//-------------------------------------------- TELE-OP PROGRAM -------------------------------------------//
 
 task main()
 {
+   waitForStart();   // wait for start of tele-op phase
 
-  waitForStart();   // wait for start of tele-op phase
+   while (true)
+   {
+      getJoystickSettings(joystick);
 
-  while (true)
-  {
-	  getJoystickSettings(joystick);
+      //---------------------------- MAIN ROBOT CONTROL ----------------------------//
 
-	  //------------MAIN ROBOT CONTROL------------//
+      //=============== LEFT JOYSTICK ===============//
 
-	  //====LEFT JOYSTICK====//
+      //*** FORWARD ***//
+      if (joystick.joy1_y1 > 10 && abs(joystick.joy1_x1) < 10 && abs(joystick.joy1_x2) < 10)
+         moveWheels(yAClock(), yClock(),  yAClock(), yClock());
 
-	  //***FORWARD***//
-	  if (joystick.joy1_y1>10 && abs(joystick.joy1_x1)<10 && abs(joystick.joy1_x2)<10)
-		{
-		  motor[frontLeft] = -abs(joystick.joy1_y1); //counterclockwise
-		  motor[frontRight] = abs(joystick.joy1_y1); //clockwise
-		  motor[rearLeft]  = -abs(joystick.joy1_y1); //counterclockwise
-		  motor[rearRight]	= abs(joystick.joy1_y1); //clockwise
-		}
+      //*** REVERSE ***//
+      if (joystick.joy1_y1 < -10 && abs(joystick.joy1_x1) < 10 && abs(joystick.joy1_x2) < 10)
+         moveWheels(yClock(), yAClock(),  yClock(), yAClock());
 
-	  //***REVERSE***//
-		if (joystick.joy1_y1<-10 && abs(joystick.joy1_x1)<10 && abs(joystick.joy1_x2)<10)
-		{
-		  motor[frontLeft] =	 abs(joystick.joy1_y1); //counterclockwise
-		  motor[frontRight] = -abs(joystick.joy1_y1); //clockwise
-		  motor[rearLeft] = 	 abs(joystick.joy1_y1); //counterclockwise
-		  motor[rearRight] =  -abs(joystick.joy1_y1); //clockwise
-		}
+      //*** STRAFE LEFT ***//
+      if (joystick.joy1_x1 > 10 && abs(joystick.joy1_y1) < 10 && abs(joystick.joy1_x2) < 10)
+         moveWheels(yClock(), yClock(),  yAClock(), yAClock());
 
-	  //***STRAFE LEFT***//
-		if (joystick.joy1_x1>10 && abs(joystick.joy1_y1)<10 && abs(joystick.joy1_x2)<10)
-		{
-		  motor[frontLeft] =  abs(joystick.joy1_x1); //counterclockwise
-		  motor[frontRight] = abs(joystick.joy1_x1); //clockwise
-		  motor[rearLeft]  = -abs(joystick.joy1_x1); //counterclockwise
-		  motor[rearRight] = -abs(joystick.joy1_x1); //clockwise
-		}
+      //*** STRAFE RIGHT ***//
+      if (joystick.joy1_x1 < -10 && abs(joystick.joy1_y1) < 10 && abs(joystick.joy1_x2) < 10)
+         moveWheels(yAClock(), yAClock(),  yClock(), yClock());
 
-	  //***STRAFE RIGHT***//
-	  if (joystick.joy1_x1<-10 && abs(joystick.joy1_y1)<10 && abs(joystick.joy1_x2)<10)
-		{
-		  motor[frontLeft] = 	-abs(joystick.joy1_x1); //counterclockwise
-		  motor[frontRight] = -abs(joystick.joy1_x1); //clockwise
-		  motor[rearLeft] 	=  abs(joystick.joy1_x1); //counterclockwise
-		  motor[rearRight] = 	 abs(joystick.joy1_x1); //clockwise
-		}
+      //*** STRAFE UP-LEFT ***//
+      if (joystick.joy1_x1 < -10 && joystick.joy1_y1 > 10 && abs(joystick.joy1_x2) < 10)
+         moveWheels(0, joyAvg(), -joyAvg(), 0);
 
-		//***STRAFE NORTHWEST***//
-		if (joystick.joy1_x1<-10 && joystick.joy1_y1>10 && abs(joystick.joy1_x2)<10)
-		{
-			motor[frontLeft] = 0;
-		  motor[frontRight] = joyDistance(); //clockwise, distance from center
-		  motor[rearLeft] = joyDistance(); //counterclockwise, distance from center
-		  motor[rearRight] = 0;
-		}
+      //*** STRAFE UP-RIGHT ***//
+      if (joystick.joy1_x1 > 10 && joystick.joy1_y1 >10 && abs(joystick.joy1_x2) < 10)
+         moveWheels(-joyAvg(), 0, 0, joyAvg());
 
-		//***STRAFE NORTHEAST***//
-		if (joystick.joy1_x1>10 && joystick.joy1_y1>10 && abs(joystick.joy1_x2)<10)
-		{
-			motor[frontLeft] = joyDistance(); //clockwise, distance from center
-		  motor[frontRight] = 0;
-		  motor[rearLeft] = 0;
-		  motor[rearRight] = joyDistance(); //counterclockwise, distance from center
-		}
+      //*** STRAFE BACK-LEFT ***//
+      if (joystick.joy1_x1 < -10 && joystick.joy1_y1 < -10 && abs(joystick.joy1_x2) < 10)
+         moveWheels(joyAvg(), 0, 0, -joyAvg());
 
-		//***STRAFE SOUTHWEST***//
-		if (joystick.joy1_x1<-10 && joystick.joy1_y1<-10 && abs(joystick.joy1_x2)<10)
-		{
-			motor[frontLeft] = joyDistance(); //clockwise, distance from center
-		  motor[frontRight] = 0;
-		  motor[rearLeft] = 0;
-		  motor[rearRight] = joyDistance(); //counterclockwise, distance from center
-		}
+      //*** STRAFE BACK-RIGHT ***//
+      if (joystick.joy1_x1 > 10 && joystick.joy1_y1 < -10 && abs(joystick.joy1_x2) < 10)
+         moveWheels(0, -joyAvg(), joyAvg(), 0);
+      //============= END LEFT JOYSTICK =============//
 
-		//***STRAFE SOUTHEAST***//
-		if (joystick.joy1_x1>10 && joystick.joy1_y1<-10 && abs(joystick.joy1_x2)<10)
-		{
-			motor[frontLeft] = 0;
-		  motor[frontRight] = joyDistance(); //clockwise, distance from center
-		  motor[rearLeft] = joyDistance(); //counterclockwise, distance from center
-		  motor[rearRight] = 0;
-		}
+      //============== RIGHT JOYSTICK ===============//
 
-		//====END LEFT JOYSTICK====//
+      //*** TURN LEFT ***//
+      if (joystick.joy1_x2 > 10 && abs(joystick.joy1_x1) < 10 && abs(joystick.joy1_y1) < 10)
+         moveWheels(abs(joystick.joy1_x2));
+
+      //*** TURN RIGHT ***//
+      if (joystick.joy1_x2 < -10 && abs(joystick.joy1_x1)<10 && abs(joystick.joy1_y1) < 10)
+         moveWheels(-abs(joystick.joy1_x2));
+      //============ END RIGHT JOYSTICK ==============//
+
+      // Stop all wheels if no joystick input
+      if (abs(joystick.joy1_x1) < 10 && abs(joystick.joy1_y1) < 10 && abs(joystick.joy1_x2) < 10)
+         moveWheels(0);
+      //--------------------------- END MAIN ROBOT CONTROL -------------------------//
 
 
-		//====RIGHT JOYSTICK====//
+      //------------------------------- LIFT CONTROL -------------------------------//
 
-		//***TURN LEFT***//
-		if (joystick.joy1_x2>10 && abs(joystick.joy1_x1)<10 && abs(joystick.joy1_y1)<10)
-		{
-		  motor[frontLeft] =  abs(joystick.joy1_x2); //clockwise
-		  motor[frontRight] = abs(joystick.joy1_x2); //clockwise
-		  motor[rearLeft]  =  abs(joystick.joy1_x2); //clockwise
-		  motor[rearRight] =  abs(joystick.joy1_x2); //clockwise
-		}
-
-	  //***TURN RIGHT***//
-	  if (joystick.joy1_x2<-10 && abs(joystick.joy1_x1)<10 && abs(joystick.joy1_y1)<10)
-		{
-		  motor[frontLeft] = 	-abs(joystick.joy1_x2); //counterclockwise
-		  motor[frontRight] = -abs(joystick.joy1_x2); //counterclockwise
-		  motor[rearLeft]  =  -abs(joystick.joy1_x2); //counterclockwise
-		  motor[rearRight] = 	-abs(joystick.joy1_x2); //counterclockwise
-		}
-
-		//====END RIGHT JOYSTICK====//
-
-		//stop all motors if no joystick input
-		if (abs(joystick.joy1_x1)<10 && abs(joystick.joy1_y1)<10 && abs(joystick.joy1_x2)<10) stopMotors();
-
-		//-----------END MAIN ROBOT CONTROL---------//
+      if (joystick.joy1_TopHat == 0) moveLift(-50);                     // Lift up
+      else if (joystick.joy1_TopHat == 4) moveLift(50);                 // Lift Down
+      else if (joystick.joy1_TopHat != 0 && joystick.joy1_TopHat != 4)  // Stop lift
+         moveLift(0);
+      //----------------------------- END LIFT CONTROL -----------------------------//
 
 
+      //------------------------------- CLAW CONTROL--------------------------------//
 
-	  //--------------LIFT CONTROL----------------//
-	  if (joystick.joy1_TopHat == 0) //move up with D-pad up
-	  {
-	  	motor[lift1] = -50;
-	  	motor[lift2] = -50;
-	  }
-	  else if (joystick.joy1_TopHat == 4) //move down with D-pad down
-	  {
-	  	motor[lift1] = 50;
-	  	motor[lift2] = 50;
-	  }
-	  else if (joystick.joy1_TopHat != 0 && joystick.joy1_TopHat != 4) //stop lift
-	  {
-	  	motor[lift1] = 0;
-	  	motor[lift2] = 0;
-	  }
-		//-------------END LIFT CONTROL-------------//
+      if (joy1Btn(5)==1) servo[claw] = 140; //open on pressing 5
+      else if (joy1Btn(6)==1) servo[claw] = 220; //close on pressing 6
+      //----------------------------- END CLAW CONTROL -----------------------------//
 
-	  //--------------CLAW CONTROL----------------//
-	  if (joy1Btn(5)==1) servo[claw] = 140; //open on pressing 5
-	  else if (joy1Btn(6)==1) servo[claw] = 220; //close on pressing 6
-	  //------------END CLAW CONTROL--------------//
 
-		//--------------LIGHTS ON/OFF---------------//
-	  //if (joy1Btn(2)==1) motor[light] = 50; //press 2 to turn on lights
-	 	//if (joy1Btn(4)==1) motor[light] = 0; //press 4 to turn off lights
-	  //------------END LIGHTS ON/OFF-------------//
+      //------------------------------ LIGHT CONTROL -------------------------------//
 
-  }
+      //if (joy1Btn(2)==1) motor[light] = 50; //press 2 to turn on lights
+      //if (joy1Btn(4)==1) motor[light] = 0; //press 4 to turn off lights
+      //---------------------------- END LIGHT CONTROL -----------------------------//
+   }
 }
